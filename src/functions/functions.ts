64f4110253554bc0654be7d1177d123c22fe83b5
Illia Negovora <illian@illiaLaptop>
2021-07-06 13:33:40 -0400

    testing rages

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -7,8 +7,8 @@
  */
 /* global clearInterval, console, setInterval */
 
-export function add(first: number[][], second: number[][]): number {
-  return first[0][0] * second[0][0];
+export function add(first: number[][]): number {
+  return first[0][0];
 }
 
 /**
