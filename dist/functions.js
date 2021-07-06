03369660f6c4391f3adcd183e09ebe5300f8f7a5
Illia Negovora <illian@illiaLaptop>
2021-07-06 14:48:49 -0400

    testing ranges

--- dist/functions.js
+++ dist/functions.js
@@ -96,147 +96,6 @@
 "use strict";
 
 
-var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
-  function adopt(value) {
-    return value instanceof P ? value : new P(function (resolve) {
-      resolve(value);
-    });
-  }
-
-  return new (P || (P = Promise))(function (resolve, reject) {
-    function fulfilled(value) {
-      try {
-        step(generator.next(value));
-      } catch (e) {
-        reject(e);
-      }
-    }
-
-    function rejected(value) {
-      try {
-        step(generator["throw"](value));
-      } catch (e) {
-        reject(e);
-      }
-    }
-
-    function step(result) {
-      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
-    }
-
-    step((generator = generator.apply(thisArg, _arguments || [])).next());
-  });
-};
-
-var __generator = this && this.__generator || function (thisArg, body) {
-  var _ = {
-    label: 0,
-    sent: function () {
-      if (t[0] & 1) throw t[1];
-      return t[1];
-    },
-    trys: [],
-    ops: []
-  },
-      f,
-      y,
-      t,
-      g;
-  return g = {
-    next: verb(0),
-    "throw": verb(1),
-    "return": verb(2)
-  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
-    return this;
-  }), g;
-
-  function verb(n) {
-    return function (v) {
-      return step([n, v]);
-    };
-  }
-
-  function step(op) {
-    if (f) throw new TypeError("Generator is already executing.");
-
-    while (_) try {
-      if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-      if (y = 0, t) op = [op[0] & 2, t.value];
-
-      switch (op[0]) {
-        case 0:
-        case 1:
-          t = op;
-          break;
-
-        case 4:
-          _.label++;
-          return {
-            value: op[1],
-            done: false
-          };
-
-        case 5:
-          _.label++;
-          y = op[1];
-          op = [0];
-          continue;
-
-        case 7:
-          op = _.ops.pop();
-
-          _.trys.pop();
-
-          continue;
-
-        default:
-          if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
-            _ = 0;
-            continue;
-          }
-
-          if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
-            _.label = op[1];
-            break;
-          }
-
-          if (op[0] === 6 && _.label < t[1]) {
-            _.label = t[1];
-            t = op;
-            break;
-          }
-
-          if (t && _.label < t[2]) {
-            _.label = t[2];
-
-            _.ops.push(op);
-
-            break;
-          }
-
-          if (t[2]) _.ops.pop();
-
-          _.trys.pop();
-
-          continue;
-      }
-
-      op = body.call(thisArg, _);
-    } catch (e) {
-      op = [6, e];
-      y = 0;
-    } finally {
-      f = t = 0;
-    }
-
-    if (op[0] & 5) throw op[1];
-    return {
-      value: op[0] ? op[1] : void 0,
-      done: true
-    };
-  }
-};
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -261,58 +120,6 @@ function addEach(range1, range2) {
 
 exports.addEach = addEach;
 /**
-  * Gets the star count for a given Github repository.
-  * @customfunction
-  * @param {string} userName string name of Github user or organization.
-  * @param {string} repoName string name of the Github repository.
-  * @return {number} number of stars given to a Github repository.
-  */
-
-function getStarCount(userName, repoName) {
-  return __awaiter(this, void 0, void 0, function () {
-    var url, response, jsonResponse, error_1;
-    return __generator(this, function (_a) {
-      switch (_a.label) {
-        case 0:
-          _a.trys.push([0, 3,, 4]);
-
-          url = "https://api.github.com/repos/" + userName + "/" + repoName;
-          return [4
-          /*yield*/
-          , fetch(url)];
-
-        case 1:
-          response = _a.sent(); //Expect that status code is in 200-299 range
-
-          if (!response.ok) {
-            throw new Error(response.statusText);
-          }
-
-          return [4
-          /*yield*/
-          , response.json()];
-
-        case 2:
-          jsonResponse = _a.sent();
-          return [2
-          /*return*/
-          , jsonResponse.watchers_count];
-
-        case 3:
-          error_1 = _a.sent();
-          return [2
-          /*return*/
-          , error_1];
-
-        case 4:
-          return [2
-          /*return*/
-          ];
-      }
-    });
-  });
-}
-/**
  * Adds two numbers.
  * @customfunction
  * @param first First number
@@ -321,7 +128,6 @@ function getStarCount(userName, repoName) {
  */
 
 /* global clearInterval, console, setInterval */
-
 
 function add(first, second) {
   return first * second;
@@ -390,7 +196,6 @@ function logMessage(message) {
 
 exports.logMessage = logMessage;
 CustomFunctions.associate("ADDEACH", addEach);
-CustomFunctions.associate("GETSTARCOUNT", getStarCount);
 CustomFunctions.associate("ADD", add);
 CustomFunctions.associate("CLOCK", clock);
 CustomFunctions.associate("INCREMENT", increment);
