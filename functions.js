d2a944fbf59d7b14553f70f10a08f0290457bc7e
Illia Negovora <illian@illiaLaptop>
2021-07-06 14:48:19 -0400

    testing ranges

--- functions.js
+++ functions.js
@@ -1,2 +1,402 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=121)}({121:function(e,t,n){"use strict";var r=this&&this.__awaiter||function(e,t,n,r){return new(n||(n=Promise))((function(o,u){function c(e){try{a(r.next(e))}catch(e){u(e)}}function i(e){try{a(r.throw(e))}catch(e){u(e)}}function a(e){var t;e.done?o(e.value):(t=e.value,t instanceof n?t:new n((function(e){e(t)}))).then(c,i)}a((r=r.apply(e,t||[])).next())}))},o=this&&this.__generator||function(e,t){var n,r,o,u,c={label:0,sent:function(){if(1&o[0])throw o[1];return o[1]},trys:[],ops:[]};return u={next:i(0),throw:i(1),return:i(2)},"function"==typeof Symbol&&(u[Symbol.iterator]=function(){return this}),u;function i(u){return function(i){return function(u){if(n)throw new TypeError("Generator is already executing.");for(;c;)try{if(n=1,r&&(o=2&u[0]?r.return:u[0]?r.throw||((o=r.return)&&o.call(r),0):r.next)&&!(o=o.call(r,u[1])).done)return o;switch(r=0,o&&(u=[2&u[0],o.value]),u[0]){case 0:case 1:o=u;break;case 4:return c.label++,{value:u[1],done:!1};case 5:c.label++,r=u[1],u=[0];continue;case 7:u=c.ops.pop(),c.trys.pop();continue;default:if(!(o=c.trys,(o=o.length>0&&o[o.length-1])||6!==u[0]&&2!==u[0])){c=0;continue}if(3===u[0]&&(!o||u[1]>o[0]&&u[1]<o[3])){c.label=u[1];break}if(6===u[0]&&c.label<o[1]){c.label=o[1],o=u;break}if(o&&c.label<o[2]){c.label=o[2],c.ops.push(u);break}o[2]&&c.ops.pop(),c.trys.pop();continue}u=t.call(e,c)}catch(e){u=[6,e],r=0}finally{n=o=0}if(5&u[0])throw u[1];return{value:u[0]?u[1]:void 0,done:!0}}([u,i])}}};function u(e,t){for(var n=0,r=0;r<e[0].length;r++)n+=e[0][r]*t[0][r];return n}function c(e,t){return e*t}function i(e){var t=setInterval((function(){var t=a();e.setResult(t)}),1e3);e.onCanceled=function(){clearInterval(t)}}function a(){return(new Date).toLocaleTimeString()}function s(e,t){var n=0,r=setInterval((function(){n+=e,t.setResult(n)}),1e3);t.onCanceled=function(){clearInterval(r)}}function l(e){return console.log(e),e}Object.defineProperty(t,"__esModule",{value:!0}),t.logMessage=t.increment=t.currentTime=t.clock=t.add=t.addEach=void 0,t.addEach=u,t.add=c,t.clock=i,t.currentTime=a,t.increment=s,t.logMessage=l,CustomFunctions.associate("ADDEACH",u),CustomFunctions.associate("GETSTARCOUNT",(function(e,t){return r(this,void 0,void 0,(function(){var n;return o(this,(function(r){switch(r.label){case 0:return r.trys.push([0,3,,4]),[4,fetch("https://api.github.com/repos/"+e+"/"+t)];case 1:if(!(n=r.sent()).ok)throw new Error(n.statusText);return[4,n.json()];case 2:return[2,r.sent().watchers_count];case 3:return[2,r.sent()];case 4:return[2]}}))}))})),CustomFunctions.associate("ADD",c),CustomFunctions.associate("CLOCK",i),CustomFunctions.associate("INCREMENT",s),CustomFunctions.associate("LOG",l)}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/functions/functions.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/functions/functions.ts":
+/*!************************************!*\
+  !*** ./src/functions/functions.ts ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
+  function adopt(value) {
+    return value instanceof P ? value : new P(function (resolve) {
+      resolve(value);
+    });
+  }
+
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+var __generator = this && this.__generator || function (thisArg, body) {
+  var _ = {
+    label: 0,
+    sent: function () {
+      if (t[0] & 1) throw t[1];
+      return t[1];
+    },
+    trys: [],
+    ops: []
+  },
+      f,
+      y,
+      t,
+      g;
+  return g = {
+    next: verb(0),
+    "throw": verb(1),
+    "return": verb(2)
+  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
+    return this;
+  }), g;
+
+  function verb(n) {
+    return function (v) {
+      return step([n, v]);
+    };
+  }
+
+  function step(op) {
+    if (f) throw new TypeError("Generator is already executing.");
+
+    while (_) try {
+      if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+      if (y = 0, t) op = [op[0] & 2, t.value];
+
+      switch (op[0]) {
+        case 0:
+        case 1:
+          t = op;
+          break;
+
+        case 4:
+          _.label++;
+          return {
+            value: op[1],
+            done: false
+          };
+
+        case 5:
+          _.label++;
+          y = op[1];
+          op = [0];
+          continue;
+
+        case 7:
+          op = _.ops.pop();
+
+          _.trys.pop();
+
+          continue;
+
+        default:
+          if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+            _ = 0;
+            continue;
+          }
+
+          if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
+            _.label = op[1];
+            break;
+          }
+
+          if (op[0] === 6 && _.label < t[1]) {
+            _.label = t[1];
+            t = op;
+            break;
+          }
+
+          if (t && _.label < t[2]) {
+            _.label = t[2];
+
+            _.ops.push(op);
+
+            break;
+          }
+
+          if (t[2]) _.ops.pop();
+
+          _.trys.pop();
+
+          continue;
+      }
+
+      op = body.call(thisArg, _);
+    } catch (e) {
+      op = [6, e];
+      y = 0;
+    } finally {
+      f = t = 0;
+    }
+
+    if (op[0] & 5) throw op[1];
+    return {
+      value: op[0] ? op[1] : void 0,
+      done: true
+    };
+  }
+};
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.logMessage = exports.increment = exports.currentTime = exports.clock = exports.add = exports.addEach = void 0;
+/**
+ *
+ * @customfunction
+ * @param range1
+ * @param range2
+ * @returns
+ */
+
+function addEach(range1, range2) {
+  var sum = 0;
+
+  for (var i = 0; i < range1[0].length; i++) {
+    sum = sum + range1[0][i] * range2[0][i];
+  }
+
+  return sum;
+}
+
+exports.addEach = addEach;
+/**
+  * Gets the star count for a given Github repository.
+  * @customfunction
+  * @param {string} userName string name of Github user or organization.
+  * @param {string} repoName string name of the Github repository.
+  * @return {number} number of stars given to a Github repository.
+  */
+
+function getStarCount(userName, repoName) {
+  return __awaiter(this, void 0, void 0, function () {
+    var url, response, jsonResponse, error_1;
+    return __generator(this, function (_a) {
+      switch (_a.label) {
+        case 0:
+          _a.trys.push([0, 3,, 4]);
+
+          url = "https://api.github.com/repos/" + userName + "/" + repoName;
+          return [4
+          /*yield*/
+          , fetch(url)];
+
+        case 1:
+          response = _a.sent(); //Expect that status code is in 200-299 range
+
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+
+          return [4
+          /*yield*/
+          , response.json()];
+
+        case 2:
+          jsonResponse = _a.sent();
+          return [2
+          /*return*/
+          , jsonResponse.watchers_count];
+
+        case 3:
+          error_1 = _a.sent();
+          return [2
+          /*return*/
+          , error_1];
+
+        case 4:
+          return [2
+          /*return*/
+          ];
+      }
+    });
+  });
+}
+/**
+ * Adds two numbers.
+ * @customfunction
+ * @param first First number
+ * @param second Second number
+ * @returns The sum of the two numbers.
+ */
+
+/* global clearInterval, console, setInterval */
+
+
+function add(first, second) {
+  return first * second;
+}
+
+exports.add = add;
+/**
+ * Displays the current time once a second.
+ * @customfunction
+ * @param invocation Custom function handler
+ */
+
+function clock(invocation) {
+  var timer = setInterval(function () {
+    var time = currentTime();
+    invocation.setResult(time);
+  }, 1000);
+
+  invocation.onCanceled = function () {
+    clearInterval(timer);
+  };
+}
+
+exports.clock = clock;
+/**
+ * Returns the current time.
+ * @returns String with the current time formatted for the current locale.
+ */
+
+function currentTime() {
+  return new Date().toLocaleTimeString();
+}
+
+exports.currentTime = currentTime;
+/**
+ * Increments a value once a second.
+ * @customfunction
+ * @param incrementBy Amount to increment
+ * @param invocation Custom function handler
+ */
+
+function increment(incrementBy, invocation) {
+  var result = 0;
+  var timer = setInterval(function () {
+    result += incrementBy;
+    invocation.setResult(result);
+  }, 1000);
+
+  invocation.onCanceled = function () {
+    clearInterval(timer);
+  };
+}
+
+exports.increment = increment;
+/**
+ * Writes a message to console.log().
+ * @customfunction LOG
+ * @param message String to write.
+ * @returns String to write.
+ */
+
+function logMessage(message) {
+  console.log(message);
+  return message;
+}
+
+exports.logMessage = logMessage;
+CustomFunctions.associate("ADDEACH", addEach);
+CustomFunctions.associate("GETSTARCOUNT", getStarCount);
+CustomFunctions.associate("ADD", add);
+CustomFunctions.associate("CLOCK", clock);
+CustomFunctions.associate("INCREMENT", increment);
+CustomFunctions.associate("LOG", logMessage);
+
+/***/ })
+
+/******/ });
 //# sourceMappingURL=functions.js.map
